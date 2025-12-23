f615b2e4dc797535142f701259de68b5b7eda8bc
Md Muinul Islam <56muin@gmail.com>
2025-12-23 23:53:04 +0600

    projectOj naming setup

---
+++ projectOj/Game.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+class Player;
+class Map;
+
+class Game {
+public:
+    static int width;
+    static int height;
+
+    enum MenuState {
+        NONE,
+        JOIN_ROOM,
+        CREATE_ROOM,
+        PLAYING
+    };
+
+    static MenuState menuState;
+    static Player* currentPlayer;
+    static Map* gameMap;
+
+    Game(int w, int h, int argc, char** argv);
+
+    static void runOpenGl(int argc, char** argv);
+    static void display();
+    static void keyPressed(unsigned char key, int x, int y);
+    static void specialKeyPressed(int key, int x, int y);
+    static void mouseMotion(int x, int y);
+    static void drawText(float x, float y, const std::string& text);
+};
---
+++ projectOj/main.cpp
@@ -0,0 +1,7 @@
+#include <GL/freeglut.h>
+#include "Game.h"
+
+int main(int argc, char** argv) {
+    Game game(800, 600, argc, argv);
+    return 0;
+}
